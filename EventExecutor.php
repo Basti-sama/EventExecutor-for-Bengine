d4362e6a6fc7b252e08265b0fe40f91641cfd1b7
sateffen <sateffen@gmail.com>
2013-09-08 17:28:04 +0200

    added project files

---
+++ EventExecutor.php
@@ -0,0 +1,28 @@
+<?php
+error_reporting(E_ALL);
+class Bengine_Game_Cronjob_EventExecutor extends Recipe_CronjobAbstract
+{
+	/**
+     * Executes all events that are queued, but max 1000 events
+     *
+     * @return Bengine_Game_Cronjob_EventExecutor
+     */
+    protected function _execute()
+    {
+    	$i = 0;
+    	$raceCondition = Str::substring(SID, 0, 8).Str::substring(md5(microtime(true)), 0, 8);
+		do
+		{
+			$collection = Application::getModel("game/event")->getCollection();
+			$collection->addRaceConditionFilter($raceCondition);
+			$collection->executeAll();
+			Core::getQuery()->delete("events", "prev_rc = ?", null, null, array($raceCondition));
+			$i++;
+		} while ($i<50 && $collection->count() > 0);
+		
+        
+        return $this;
+    }
+}
+
+?>
--- README.md
+++ README.md
@@ -2,3 +2,24 @@ EventExecutor-for-Bengine
 =========================
 
 An cronjob for the bengine, that executes all leftover events
+
+
+
+Requirements
+============
+
+To use this plugin you'll need to use at least the bengine at version 0.3.
+
+
+
+Installation
+============
+
+To install this plugin execute the EventExecutor.sql to your database. Then put the EventExecutor.php file in the /app/code/Bengine/Game/Cronjob directory and the installation is finished.
+
+
+
+Bugs
+====
+
+Please report any kind of bugs to https://github.com/sateffen/EventExecutor-for-Bengine/issues
